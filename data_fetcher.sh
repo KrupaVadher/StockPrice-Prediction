a1159837c86f49968cd0123b500dbeb2b802a13d
Krupa Vadher <krupavadher5@gmail.com>
2020-03-20 00:52:51 -0700

    update data_fetcher to actually work

--- data_fetcher.sh
+++ data_fetcher.sh
@@ -1,10 +1,14 @@
-#/bin/bash
+#!/bin/dash
 
 #download dis share prices
-curl https://query1.finance.yahoo.com/v7/finance/download/DIS?period1=-252374400&period2=1584576000&interval=1d&events=history -o dis.csv &
+DIS="curl https://query1.finance.yahoo.com/v7/finance/download/DIS?period1=-252374400&period2=1584576000&interval=1d&events=history -o dis.csv"
+`$DIS` 
 #download aapl share prices
-curl https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=345427200&period2=1584576000&interval=1d&events=history -o aapl.csv &
+AAPL="curl https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=345427200&period2=1584576000&interval=1d&events=history -o aapl.csv"
+`$AAPL`
 #download tsla share prices
-curl https://query1.finance.yahoo.com/v7/finance/download/TSLA?period1=1277769600&period2=1584576000&interval=1d&events=history -o tsla.csv &
+TSLA="curl https://query1.finance.yahoo.com/v7/finance/download/TSLA?period1=1277769600&period2=1584576000&interval=1d&events=history -o tsla.csv"
+`$TSLA`
 #download amd share prices
-curl https://query1.finance.yahoo.com/v7/finance/download/AMD?period1=322099200&period2=1584576000&interval=1d&events=history -o amd.csv
+AMD="curl https://query1.finance.yahoo.com/v7/finance/download/AMD?period1=322099200&period2=1584576000&interval=1d&events=history -o amd.csv"
+`$AMD`
